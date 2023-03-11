5a182d16928edf8fa442c229c9eca6b789cf01f5
timothymichaelcook <tmc295@email.vccs.edu>
2023-03-11 08:46:01 -0500

    created quiz and dom variables in logic.js file, created comments as well for sections of code, outlined as variables, functions, and event listeners in this order

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,19 +1,42 @@
-// Declare variables to keep track of quiz state
+
+// Declare variables section
+// Declare quiz variables
+var currentQuestionIndex = 0;
+var time = questions.length * 15;
+var timerId;
+
+
+// Declare DOM element variables using getElementById method
+var questionsElement = document.getElementById('questions');
+var timerElement = document.getElementById('time');
+var choicesElement = document.getElementById('choices');
+var submitButton = document.getElementById('submit');
+var startButton = document.getElementById('start');
+var initialsElement = document.getElementById('initials');
+var feedbackElement = document.getElementById('feedback');
 
 
 
-// Declare variable to reference DOM(Browser API) elements
 
 
-// Declare function for starting quiz as function declaration
-
-
-
-// Declare function for importing questions as function declaration
+// Declare functions section
 
 
 
 
-// Event listerners
 
 
+
+
+
+// User clicks this button to start quiz
+startButton.onclick = startQuiz;
+
+// User clicks this button to save score
+submitButton.onclick = saveHighScore;
+
+// User clicks on button containing choices
+choicesElement.onclick = questionClick;
+
+// User enters initials, saves when enter key is pressed
+initialsElement.onkeyup = checkForEnter;
